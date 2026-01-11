f88c470c1b291c0f0a751009332776629a8a780d
Vamshi Krishna <145258844+Vamshikrishan@users.noreply.github.com>
2026-01-11 08:54:17 +0000

    fix the errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Pharmacy from "./pages/Pharmacy";
 import Patient from "./pages/Patient";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pharmacy" element={<Pharmacy />} />
-      <Route path="/patient" element={<Patient />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pharmacy" element={<Pharmacy />} />
+        <Route path="/patient" element={<Patient />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
