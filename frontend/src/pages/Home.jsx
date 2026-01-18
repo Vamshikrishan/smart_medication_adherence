a02d8bddecb9389bc035fe789a24d06647fe0843
Vamshi Krishna <145258844+Vamshikrishan@users.noreply.github.com>
2026-01-18 11:41:30 +0000

    adding styles for pharmacy page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,19 +2,22 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
-
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>QR-Based Smart Medication Adherence System</h1>
+    <div className="landing-container">
+      <div className="landing-content">
+        <h1 className="landing-title">
+          QR-Based Smart Medication <br /> Adherence System
+        </h1>
 
-      <div style={{ marginTop: "40px" }}>
-        <button onClick={() => navigate("/pharmacy")} style={{ marginRight: "20px" }}>
-          Pharmacy
-        </button>
+        <div className="landing-buttons">
+          <button className="btn btn-primary landing-btn" onClick={() => navigate("/pharmacy")}>
+            Pharmacy
+          </button>
 
-        <button onClick={() => navigate("/patient")}>
-          Patient
-        </button>
+          <button className="btn btn-primary landing-btn" onClick={() => navigate("/patient")}>
+            Patient
+          </button>
+        </div>
       </div>
     </div>
   );
